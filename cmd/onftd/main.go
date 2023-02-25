280cc31c3b9fa4f63109e4ccc6005ed0e583d265
Marri Harish <harishmarri551@gmail.com>
2023-02-25 11:06:08 +0530

    bump up cosmos-sdk@v0.46.x (#20)
    * add protoc scripts
    
    * update to cosmos-sdk@v0.46.x
    
    * Update build.yml

--- cmd/onftd/main.go
+++ cmd/onftd/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"os"
 
-	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/OmniFlix/onft/app"
 	"github.com/OmniFlix/onft/cmd/onftd/cmd"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
 
 func main() {
 	rootCmd, _ := cmd.NewRootCmd()
-	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+	if err := svrcmd.Execute(rootCmd, "ONFTD", app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
 }
--- keeper/keeper_test.go
+++ keeper/keeper_test.go
@@ -3,12 +3,12 @@ package keeper
 import (
 	"testing"
 
+	"github.com/OmniFlix/onft/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/OmniFlix/onft/types"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -21,8 +21,8 @@ func setupKeeper(t testing.TB) (*Keeper, sdk.Context) {
 
 	db := tmdb.NewMemDB()
 	stateStore := store.NewCommitMultiStore(db)
-	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
-	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
+	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(memStoreKey, storetypes.StoreTypeMemory, nil)
 	require.NoError(t, stateStore.LoadLatestVersion())
 
 	registry := codectypes.NewInterfaceRegistry()
