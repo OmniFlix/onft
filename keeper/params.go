d795949fede8578db9a0e1c200700ed3786e3244
Harish Marri <harishmarri551@gmail.com>
2023-06-15 01:34:25 +0530

    update comments

--- keeper/params.go
+++ keeper/params.go
@@ -5,13 +5,13 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// GetParams gets the parameters for the marketplace module.
+// GetParams gets the parameters for the onft module.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 	k.paramSpace.GetParamSet(ctx, &params)
 	return params
 }
 
-// SetParams sets the parameters for the marketplace module.
+// SetParams sets the parameters for the onft module.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramSpace.SetParamSet(ctx, &params)
 }
